53c60529b6038f31a859d849767e37760ea64603
Selim Ajimi <sloumma.21@gmail.com>
2018-02-03 19:19:41 +0100

    Add BackBundle

---
+++ src/Back/BackBundle/Controller/TopNavController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Back\BackBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class TopNavController
+ * @package Back\BackBundle\Controller
+ * @Route("/admin")
+ */
+class TopNavController extends Controller
+{
+    /**
+     * @Route("/profile", name="app_profile")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function profileAction(Request $request)
+    {
+        return $this->render('@Back/app/pages/profile.html.twig', []);
+    }
+}
