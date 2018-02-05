b1bc0721e6e05dd8c16e32d5abd69dadb3bf4c66
Selim Ajimi <sloumma.21@gmail.com>
2018-02-05 14:39:37 +0100

    Add front Bundle

---
+++ src/Front/FrontBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Front\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('FrontBundle:main:main.html.twig');
+    }
+}
