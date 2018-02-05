1b7461eee7e8d91f2a8ab8553e7403ffd57acbe5
Selim Ajimi <sloumma.21@gmail.com>
2018-02-05 16:52:59 +0100

    Add render to DefaultController

--- src/Front/FrontBundle/Controller/DefaultController.php
+++ src/Front/FrontBundle/Controller/DefaultController.php
@@ -4,14 +4,27 @@ namespace Front\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('FrontBundle:main:main.html.twig');
+        // replace this example code with whatever you need
+        return $this->render('FrontBundle:main:main.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/news", name="home")
+     */
+    public function newsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('FrontBundle:main:news.html.twig', [
+        ]);
     }
 }
