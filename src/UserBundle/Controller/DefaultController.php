2a765cf95e5886541fe3c9da032e8f9bb15ddc7c
Selim Ajimi <sloumma.21@gmail.com>
2018-02-06 20:54:03 +0100

    Fix name space

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace User\UserBundle\Controller;
+namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
