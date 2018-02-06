d4f608c1572505e6c4cadac759b1762f6a3ff563
Selim Ajimi <sloumma.21@gmail.com>
2018-02-06 19:31:38 +0100

    Refactor TeamBundle to work with annotation

--- src/TeamBundle/Controller/TeamController.php
+++ src/TeamBundle/Controller/TeamController.php
@@ -2,21 +2,27 @@
 
 namespace TeamBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TeamController extends Controller
 {
-    public function DisplayAction($Name)
+    /**
+     * @Route("/display/{Name}", name="team_display")
+     */
+    public function displayAction($Name)
     {
         return $this->render('TeamBundle:Team:display.html.twig', array(
             'name'=>$Name
         ));
     }
 
-    public function ListAction()
+    /**
+     * @Route("/list", name="team_list")
+     */
+    public function listAction()
     {
-        return $this->render('TeamBundle:Team:list.html.twig', array(
-            // ...
+        return $this->render('TeamBundle:Team:list.html.twig', array(// ...
         ));
     }
 
