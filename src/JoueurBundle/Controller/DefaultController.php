b2ab06766e3d940ffe2827b6a73b8cce16c30c95
Selim Ajimi <sloumma.21@gmail.com>
2018-02-06 19:24:40 +0100

    Change Joueur Bundle to annotation

--- src/JoueurBundle/Controller/DefaultController.php
+++ src/JoueurBundle/Controller/DefaultController.php
@@ -2,10 +2,14 @@
 
 namespace JoueurBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="joueur_homepage")
+     */
     public function indexAction()
     {
         return $this->render('JoueurBundle:Default:index.html.twig');
--- src/JoueurBundle/Controller/JoueurController.php
+++ src/JoueurBundle/Controller/JoueurController.php
@@ -4,9 +4,14 @@ namespace JoueurBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class JoueurController extends Controller
 {
+
+    /**
+     * @Route("/display", name="joueur_Display")
+     */
     public function DisplayAction()
     {
         return $this->render('JoueurBundle:Joueur:display.html.twig', array(
