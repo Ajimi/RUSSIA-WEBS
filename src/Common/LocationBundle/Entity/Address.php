be564d7d1de7fcf29a07e2d54b11d1b5e6e691d2
Selim Ajimi <sloumma.21@gmail.com>
2018-02-10 17:32:58 +0100

    Add Address entity

---
+++ src/Common/LocationBundle/Entity/Address.php
@@ -0,0 +1,247 @@
+<?php
+
+namespace Common\LocationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table(name="address")
+ * @ORM\Entity(repositoryClass="Common\LocationBundle\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255, nullable=true)
+     */
+    private $country;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="state", type="string", length=255, nullable=true)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street1", type="string", length=255, nullable=true)
+     */
+    private $street1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street2", type="string", length=255, nullable=true)
+     */
+    private $street2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postalcode", type="string", length=255, nullable=true)
+     */
+    private $postalcode;
+
+    /**
+     * One Address has One Location.
+     * @ORM\OneToOne(targetEntity="Location", inversedBy="address")
+     * @ORM\JoinColumn(name="address_id", referencedColumnName="id")
+     */
+    private $location;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get postalcode
+     *
+     * @return string
+     */
+    public function getPostalcode()
+    {
+        return $this->postalcode;
+    }
+
+    /**
+     * Set postalcode
+     *
+     * @param string $postalcode
+     *
+     * @return Address
+     */
+    public function setPostalcode($postalcode)
+    {
+        $this->postalcode = $postalcode;
+
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set state
+     *
+     * @param string $state
+     *
+     * @return Address
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get street1
+     *
+     * @return string
+     */
+    public function getStreet1()
+    {
+        return $this->street1;
+    }
+
+    /**
+     * Set street1
+     *
+     * @param string $street1
+     *
+     * @return Address
+     */
+    public function setStreet1($street1)
+    {
+        $this->street1 = $street1;
+
+        return $this;
+    }
+
+    /**
+     * Get street2
+     *
+     * @return string
+     */
+    public function getStreet2()
+    {
+        return $this->street2;
+    }
+
+    /**
+     * Set street2
+     *
+     * @param string $street2
+     *
+     * @return Address
+     */
+    public function setStreet2($street2)
+    {
+        $this->street2 = $street2;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Address
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param Location $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+}
