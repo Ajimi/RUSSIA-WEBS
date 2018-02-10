f0f2c4f29511adf90ac0a5411fdfb638704537b6
Selim Ajimi <sloumma.21@gmail.com>
2018-02-10 17:33:14 +0100

    Add GeoCode Entity

---
+++ src/Common/LocationBundle/Entity/GeoCode.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Common\LocationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * GeoCode
+ *
+ * @ORM\Table(name="geo_code")
+ * @ORM\Entity(repositoryClass="Common\LocationBundle\Repository\GeoCodeRepository")
+ */
+class GeoCode
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="longitude", type="decimal", precision=10, scale=8, nullable=true)
+     */
+    private $longitude;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="latitude", type="decimal", precision=10, scale=8, nullable=true)
+     */
+    private $latitude;
+
+    /**
+     * One GeoCode has One Location.
+     * @ORM\OneToOne(targetEntity="Location", inversedBy="geoCode")
+     * @ORM\JoinColumn(name="geocode_id", referencedColumnName="id")
+     */
+    private $location;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return string
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param string $longitude
+     *
+     * @return GeoCode
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return string
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param string $latitude
+     *
+     * @return GeoCode
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param Location $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+}
