8108d07d98c52694a1b3824f696967e2a469df2a
Ahlem Ben Guiza <ahlembenguiza@gmail.com>
2018-02-11 17:25:39 +0100

    Rename DefaultController to MathController

--- src/Match/MatchBundle/Controller/MatchController.php
+++ src/Match/MatchBundle/Controller/MatchController.php
@@ -3,9 +3,18 @@
 namespace Match\MatchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+
+
+/**
+ * @Route("/match")
+ */
+class MatchController extends Controller
 {
+    /**
+     * @Route("/index", name="match_index")
+     */
     public function indexAction()
     {
         return $this->render('MatchBundle:Default:index.html.twig');
