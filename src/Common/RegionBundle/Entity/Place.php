2ead92f0f7ea6cafdc21e92b1d66b897d9c2b21a
Selim Ajimi <sloumma.21@gmail.com>
2018-02-13 21:00:44 +0100

    Add Place Entity

---
+++ src/Common/RegionBundle/Entity/Place.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Common\RegionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Place
+ *
+ * @ORM\Table(name="place")
+ * @ORM\Entity(repositoryClass="Common\RegionBundle\Repository\PlaceRepository")
+ */
+class Place
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255, nullable=true)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="information", type="string", length=255, nullable=true)
+     */
+    private $information;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Place
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Place
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get information
+     *
+     * @return string
+     */
+    public function getInformation()
+    {
+        return $this->information;
+    }
+
+    /**
+     * Set information
+     *
+     * @param string $information
+     *
+     * @return Place
+     */
+    public function setInformation($information)
+    {
+        $this->information = $information;
+
+        return $this;
+    }
+}
+
