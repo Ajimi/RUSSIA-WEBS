26ac23b02377f53e18852aa92a455b2cf3d27fe2
Selim Ajimi <sloumma.21@gmail.com>
2018-02-14 09:25:08 +0100

    Add Event Listener for doctrine changes

---
+++ src/Common/UploadBundle/EventListener/UploadSubscriber.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hir0w
+ * Date: 2/14/2018
+ * Time: 9:11 AM
+ */
+
+namespace Common\UploadBundle\EventListener;
+
+
+use Common\UploadBundle\Annotation\UploadAnnotationReader;
+use Common\UploadBundle\Handler\UploadHandler;
+use Doctrine\Common\EventArgs;
+use Doctrine\Common\EventSubscriber;
+
+/**
+ * Class UploadSubscriber
+ * @package Common\UploadBundle\EventListener
+ */
+class UploadSubscriber implements EventSubscriber
+{
+
+    /**
+     * @var UploadAnnotationReader
+     */
+    private $reader;
+
+    /**
+     * @var UploadHandler
+     */
+    private $handler;
+
+    public function __construct(UploadAnnotationReader $reader, UploadHandler $handler)
+    {
+        $this->reader = $reader;
+        $this->handler = $handler;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [
+            'prePersist',
+            'preUpdate',
+            'postLoad',
+            'postRemove'
+        ];
+    }
+
+    public function prePersist(EventArgs $event)
+    {
+        $this->preEvent($event);
+    }
+
+    private function preEvent(EventArgs $event)
+    {
+        $entity = $event->getEntity();
+        foreach ($this->reader->getUploadableFields($entity) as $property => $annotation) {
+            $this->handler->uploadFile($entity, $property, $annotation);
+        }
+    }
+
+    public function preUpdate(EventArgs $event)
+    {
+        $this->preEvent($event);
+    }
+
+    public function postLoad(EventArgs $event)
+    {
+        $entity = $event->getEntity();
+        foreach ($this->reader->getUploadableFields($entity) as $property => $annotation) {
+            $this->handler->setFileFromFilename($entity, $property, $annotation);
+        }
+    }
+
+    public function postRemove(EventArgs $event)
+    {
+        $entity = $event->getEntity();
+        foreach ($this->reader->getUploadableFields($entity) as $property => $annotation) {
+            $this->handler->removeFile($entity, $property);
+        }
+    }
+}
