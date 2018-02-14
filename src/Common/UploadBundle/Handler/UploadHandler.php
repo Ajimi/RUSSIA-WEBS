b058f77067d2b0f6be2e0fe0f23ba88d35d4262b
Selim Ajimi <sloumma.21@gmail.com>
2018-02-14 09:26:05 +0100

    Add UploadHandler

---
+++ src/Common/UploadBundle/Handler/UploadHandler.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hir0w
+ * Date: 2/14/2018
+ * Time: 9:13 AM
+ */
+
+namespace Common\UploadBundle\Handler;
+
+
+class UploadHandler
+{
+
+    private $accessor;
+
+    public function __construct()
+    {
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    public function uploadFile($entity, $property, $annotation)
+    {
+        $file = $this->accessor->getValue($entity, $property);
+        if ($file instanceof UploadedFile) {
+            $this->removeOldFile($entity, $annotation);
+            $filename = $file->getClientOriginalName();
+            $file->move($annotation->getPath(), $filename);
+            $this->accessor->setValue($entity, $annotation->getFilename(), $filename);
+        }
+    }
+
+    public function removeOldFile($entity, $annotation)
+    {
+        $file = $this->getFileFromFilename($entity, $annotation);
+        if ($file !== null) {
+            @unlink($file->getRealPath());
+        }
+    }
+
+    private function getFileFromFilename($entity, $annotation)
+    {
+        $filename = $this->accessor->getValue($entity, $annotation->getFilename());
+        if (empty($filename)) {
+            return null;
+        } else {
+            return new File($annotation->getPath() . DIRECTORY_SEPARATOR . $filename, false);
+        }
+    }
+
+    public function setFileFromFilename($entity, $property, $annotation)
+    {
+        $file = $this->getFileFromFilename($entity, $annotation);
+        $this->accessor->setValue($entity, $property, $file);
+    }
+
+    public function removeFile($entity, $property)
+    {
+        $file = $this->accessor->getValue($entity, $property);
+        if ($file instanceof File) {
+            @unlink($file->getRealPath());
+        }
+    }
+
+}
