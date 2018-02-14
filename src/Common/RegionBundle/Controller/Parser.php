f46a46ff04a505e7bdf8c0d62aa8eec94c550d14
Selim Ajimi <sloumma.21@gmail.com>
2018-02-14 13:15:00 +0100

    Create A controller

---
+++ src/Common/RegionBundle/Controller/Parser.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hir0w
+ * Date: 2/14/2018
+ * Time: 11:23 AM
+ */
+
+namespace Common\RegionBundle\Controller;
+
+use Common\RegionBundle\Parser\PlaceParser;
+use Common\RegionBundle\RegionBundle;
+use GuzzleHttp\Client;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/**
+ * Class Parser
+ * @package Common\RegionBundle\Controller
+ * @Route("parser")
+ */
+class Parser extends Controller
+{
+
+
+    /**
+     * @Route("/{id}/{city}")
+     * @throws \InvalidArgumentException
+     */
+    public function parse(Request $request, $id, $city)
+    {
+
+        $placeParser = PlaceParser::createPlaceParser($this->getDoctrine()->getManager());
+        $placeParser->createPlaces($id, $city);
+        return new Response("Haha");
+    }
+}
