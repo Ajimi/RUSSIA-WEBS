c65c85839b4874cfe7295aa68a1182bdc5d488d2
Ahlem Ben Guiza <ahlembenguiza@gmail.com>
2018-02-17 12:39:13 +0100

    Add create match action

--- src/Match/MatchBundle/Controller/MatchBackController.php
+++ src/Match/MatchBundle/Controller/MatchBackController.php
@@ -17,9 +17,30 @@ class MatchBackController extends Controller
     /**
      * @Route("/index", name="match_index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('MatchBundle:Default:index.html.twig');
+        $match = new Match();
+        $form = $this->createForm(MatchType::class, $match);
+        $form->handleRequest($request);
+        if ($form->isValid() & $form->isSubmitted()) {
+            dump($form->getData());
+
+            dump($match);
+            $em = $this->getDoctrine()->getManager();
+            $match->setDate($request->get('calendar'));
+            $match->setTime($request->get('timepicker'));
+
+            dump($match);
+
+            $em->persist($match);
+            $em->flush();
+            // return $this->redirectToRoute('_afficheV');
+        }
+
+        return $this->render('MatchBundle:Default:index.html.twig', [
+            'matchForm' => $form->createView()
+        ]);
+
     }
 
     /**
