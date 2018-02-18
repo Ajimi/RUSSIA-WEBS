deae955907454d4f6fc4eaf724732a78765a83f6
Moncef Guettat <ktatmoncef01@gmail.com>
2018-02-18 17:03:18 +0100

    Add Player Feature Forms

---
+++ src/Player/PlayerBundle/Form/PlayerType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Player\PlayerBundle\Form;
+
+use Doctrine\DBAL\Types\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('playerName')->add('playerLastName')
+            ->add('playerImage')->add('playerPosition')
+            ->add('birthday')->add('weight')->add('height')
+            ->add('totalGames')->add('bio')->add('team')
+            ->add('goalScored')->add('shots')->add('shotsOnTarget')
+            ->add('assists')->add('passes')->add('fouls')
+            ->add('minutesPlayed')->add('yellowCard')
+            ->add('redCard')
+            ->add('nationalTeam',EntityType::class,array(
+                'class' => 'TeamBundle:Team',
+                'choice_label' => 'teamName', 'multiple' => false))
+            ->add('create', SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Player\PlayerBundle\Entity\Player'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'player_playerbundle_player';
+    }
+
+
+}
