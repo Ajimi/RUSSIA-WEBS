f3f8e255470bdd390c1588c558862ff65d84f550
Ahlem Ben Guiza <ahlembenguiza@gmail.com>
2018-02-21 21:23:12 +0100

    Fix Display Action

--- src/Match/MatchBundle/Controller/MatchFrontController.php
+++ src/Match/MatchBundle/Controller/MatchFrontController.php
@@ -22,8 +22,10 @@ class MatchFrontController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $matchs = $em->getRepository("MatchBundle:Match")->findBy(array('played'=>false));
+        $scores = $em->getRepository("MatchBundle:Score")->findThree();
         return $this->render('@Match/FrontViews/game_schedule.html.twig',array(
-            'matchs'=>$matchs
+            'matchs'=>$matchs,
+            'scores'=>$scores
         ));
     }
 
