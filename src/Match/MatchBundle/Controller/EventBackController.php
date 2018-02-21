a2cb76be1a31a119a8a548fb6a84af102c5169d8
Ahlem Ben Guiza <ahlembenguiza@gmail.com>
2018-02-21 13:08:44 +0100

    Add start and end Game Actions

--- src/Match/MatchBundle/Controller/EventBackController.php
+++ src/Match/MatchBundle/Controller/EventBackController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 class EventBackController extends Controller
 {
     /**
-     * @Route("/add{idm}", name="add_event")
+     * @Route("/add/{idm}", name="add_event")
      */
     public function addAction(Request $request, $idm)
     {
@@ -30,11 +30,12 @@ class EventBackController extends Controller
             $team = $em->getRepository('MatchBundle:TeamTest')->find($request->get('iCheck'));
             $event->setMatch($match);
             $event->setTeam($team);
+            $event->setTimes(new \DateTime());
             $em->persist($event);
             $em->flush();
         }
 
-        $events = $em->getRepository("MatchBundle:Event")->findBy(array('match' => $idm));
+        $events = $em->getRepository("MatchBundle:Event")->findBy(array('match' => $idm), array('times' => 'asc'));
         return $this->render('MatchBundle:EventBack:add_event_form.html.twig', array(
             'eventForm' => $form->createView(),
             'team1' => $match->getTeam1(),
@@ -42,8 +43,50 @@ class EventBackController extends Controller
             'idm' => $idm,
             'events' => $events
         ));
+    }
+
+
+    /**
+     * @Route("/start/{idm}", name="start_game")
+     */
+    public function startGameAction($idm)
+    {
+        $event = new Event();
+        $em = $this->getDoctrine()->getManager();
+        $event->setMatch($em->getRepository("MatchBundle:Match")->find($idm));
+        $event->setTimes(new \DateTime());
+        $event->setMinutes(0);
+        $event->setTypeEvent("START OF THE MATCH");
+        $em->persist($event);
+        $em->flush();
+        return $this->redirectToRoute('add_event', array(
+            'idm' => $idm));
+    }
+
+
+    /**
+     * @Route("/end/{idm}", name="end_game")
+     */
+
+    public function endGameAction($idm)
+    {
+        $event = new Event();
+        $em = $this->getDoctrine()->getManager();
+        $event->setMatch($em->getRepository("MatchBundle:Match")->find($idm));
+        $event->setTimes(new \DateTime());
+        $start = $em->getRepository('MatchBundle:Event')->findOneBy(array('match' => $idm, 'minutes' => 0));
+        $diff = date_diff(new \DateTime(), $start->getTimes());
+        $event->setMinutes($diff->format('%i minute'));
+        $event->setTypeEvent("END OF THE GAME");
+        $em->persist($event);
+        $em->flush();
+        return $this->redirectToRoute('add_event', array(
+            'idm' => $idm));
+
 
     }
+
+
 
 
 }
