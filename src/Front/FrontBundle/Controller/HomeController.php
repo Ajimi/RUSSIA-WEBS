7a26339487e2a887f0e76c67445237ed3cb192cd
Ahlem Ben Guiza <ahlembenguiza@gmail.com>
2018-02-26 19:18:06 +0100

    Fix standings display in Home page

--- src/Front/FrontBundle/Controller/HomeController.php
+++ src/Front/FrontBundle/Controller/HomeController.php
@@ -31,18 +31,35 @@ class HomeController extends Controller
     public function displayStandings()
     {
         $em = $this->getDoctrine()->getManager();
-        $teams = $em->getRepository('TeamBundle:Team')->findAll();
-        dump($teams);
+        $g = $em->getRepository('GroupBundle:Groupe')->findOneBy(array('name'=>'A'));
+
+        /** @var StandingsFormat $standings */
         $standings = [];
 
-        foreach ($teams as $t) {
-            $standingFormat = new StandingsFormat();
-            $standingFormat->dataFormat($t);
-            array_push($standings, $standingFormat);
-        }
+        $s1 = new StandingsFormat();
+        $s2 = new StandingsFormat();
+        $s3 = new StandingsFormat();
+        $s4 = new StandingsFormat();
+
+        $s1->setGroup($g->getName());
+        $s2->setGroup($g->getName());
+        $s3->setGroup($g->getName());
+        $s4->setGroup($g->getName());
+
+        $s1->dataFormat($g->getTeam1());
+        dump($g->getTeam1());
+        array_push($standings, $s1);
+        $s2->dataFormat($g->getTeam2());
+        array_push($standings, $s2);
+        $s3->dataFormat($g->getTeam3());
+        array_push($standings, $s3);
+        $s4->dataFormat($g->getTeam4());
+        array_push($standings, $s4);
+
         usort($standings, function ($a, $b) {
-            return ($a->points < $b->points);
+            return $a->points < $b->points;
         });
+
         return $standings;
 
     }
