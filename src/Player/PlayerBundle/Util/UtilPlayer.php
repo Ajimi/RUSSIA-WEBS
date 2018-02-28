21799da1854b5e2e06f1d60f6c6270cfd65a936e
Moncef Guettat <ktatmoncef01@gmail.com>
2018-02-28 22:39:51 +0100

    add Util Class

---
+++ src/Player/PlayerBundle/Util/UtilPlayer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KaYdaz
+ * Date: 28/02/2018
+ * Time: 22:31
+ */
+
+namespace Player\PlayerBundle\Util;
+
+
+class UtilPlayer
+{
+    function playerSkills($player, &$shotAcc, &$finishing)
+    {
+        if ($player . shotsOnTarget != 0)
+            $finishing = $player . goalScored * 100 / $player . shotsOnTarget;
+        else
+            $finishing = 0;
+        if ($player . shots != 0)
+            $shotAcc = $player . shotsOnTarget * 100 / $player . shots;
+        else
+            $shotAcc = 0;
+    }
+}
