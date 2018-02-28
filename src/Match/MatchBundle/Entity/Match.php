405cdcdfec5a3df5126241519368883dc2fa539f
Selim Ajimi <sloumma.21@gmail.com>
2018-02-28 20:07:58 +0100

    Add matches and update teams fixtures

--- src/Match/MatchBundle/Entity/Match.php
+++ src/Match/MatchBundle/Entity/Match.php
@@ -76,7 +76,7 @@ class Match
     /**
      * @var Ticket
      *
-     * @ORM\OneToOne(targetEntity="Reservation\TicketBundle\Entity\Ticket" , mappedBy="match", cascade={"remove"})
+     * @ORM\OneToOne(targetEntity="Reservation\TicketBundle\Entity\Ticket" , mappedBy="match", cascade={"remove", "persist" })
      */
     private $ticket;
 
